32d7671614a6a0034f2b4faf35b4af7378a3b011
Michael Stillwell <mjs@beebo.org>
2019-03-25 17:09:05 +0000

    Add comment

--- amp-toolbox-linter/src/cli.ts
+++ amp-toolbox-linter/src/cli.ts
@@ -1,3 +1,7 @@
+// Note: this should only be used for local testing; the package binary is
+// constructed by @pika/plugin-simple-bin; see
+// https://github.com/pikapkg/builders/tree/master/packages/plugin-simple-bin.
+
 import { cli } from ".";
 
 cli(process.argv).catch(function(error: any) {

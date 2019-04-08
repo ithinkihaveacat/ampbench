31183a87f06e2680a97f7c304d71064ea468dc54
Michael Stillwell <mjs@beebo.org>
2019-04-08 17:38:30 +0100

    Get version in more compatible way

--- amp-toolbox-linter/src/cli.ts
+++ amp-toolbox-linter/src/cli.ts
@@ -7,6 +7,8 @@ import { testsForType, lint, Message } from ".";
 import { fetchToCurl } from "./url";
 import { isArray } from "util";
 
+import { version } from "../package.json";
+
 const UA = {
   googlebot_mobile: [
     "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36",
@@ -29,7 +31,7 @@ const UA = {
 
 export function cli(argv: string[]) {
   program
-    .version(require("../package.json").version)
+    .version(version)
     .usage(`amplint [options] URL|copy_as_cURL`)
     .option(
       `-f, --force <string>`,
